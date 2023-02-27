c1f852bda37631e37a88f474ba272c3c022e02d4
maitreya-v <maitreya.vaghulade@gmail.com>
2023-02-27 14:21:33 +0530

    changes removed from App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-//hehe
-
-
-YOOO
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
